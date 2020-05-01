0db8112955827cb095cb6f70c234d59cda8497ea
Miguel Castillo <miguelcastillo1992lp@gmail.com>
2020-05-01 12:56:37 -0300

    Beans y views

---
+++ src/beans/ListadosBean.java
@@ -0,0 +1,50 @@
+package beans;
+
+import java.util.List;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+import utils.Factory;
+import model.Actividad;
+import model.Dificultad;
+import model.Formato;
+import model.Privacidad;
+import model.Ruta;
+import model.Usuario;
+
+@ManagedBean(name = "ListadosBean")
+@RequestScoped
+public class ListadosBean {
+   
+   public ListadosBean() {
+		super();
+   }
+   
+   public List<Actividad> getActividades() {
+	return Factory.daoActividad().listar();
+   }
+   
+   public List<Actividad> getActividadesHabilitadas() {
+	return Factory.daoActividad().listarHabilitadas();
+   }
+   
+   public List<Dificultad> getDificultades() {
+	return Factory.daoDificultad().listar();
+   }
+
+   public List<Formato> getFormatos() {
+	return Factory.daoFormato().listar();
+   }
+
+   public List<Privacidad> getPrivacidades(){
+	   return Factory.daoPrivacidad().listar();
+   }
+   
+   public List<Ruta> getMisRutas(){
+	   return Factory.daoRuta().listar();
+   }
+   
+   public List<Usuario> getUsuarios(){
+	   return Factory.daoUsuario().listar();
+   }
+     
+}
---
+++ src/beans/OutcomeBean.java
@@ -0,0 +1,62 @@
+package beans;
+
+import javax.faces.bean.ApplicationScoped;
+import javax.faces.bean.ManagedBean;
+
+@ManagedBean(name = "OutcomeBean")
+@ApplicationScoped
+public class OutcomeBean {
+
+	public OutcomeBean() {
+		super();
+	}
+	
+	/* HOME */
+	public String home(){
+		// agregar logica para que redireccione al home que corresponda al tipo de usuario
+		return "home";
+	}
+	
+	/* ABM RUTA */
+	public String misRutas(){
+		return "mis_rutas";
+	}
+
+	public String nuevaRuta(){
+		return "nueva_ruta";
+	}
+	
+	public String editarRuta(){
+		return "editar_ruta";
+	}
+	
+	public String detalleRuta(){
+		return "detalle_ruta";
+	}
+	
+	/* USUARIOS Y ADMINISTRADORES */
+	
+	public String usuarios() {
+		return "usuarios";
+	}
+	
+	public String editarUsuario() {
+		return "editar_usuario";
+	}
+	
+	/* ACTIVIDADES */
+	
+	public String nuevaActividad(){
+		return "nueva_actividad";
+	}
+	
+	public String editarActividad(){
+		return "editar_actividad";
+	}
+	
+	public String actividades(){
+		return "actividades";
+	}
+	
+
+}
